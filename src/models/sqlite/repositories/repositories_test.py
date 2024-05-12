15f84f6bd234ad1214d68b369c96a07250a840df
VictorGianelli <victorgnll@gmail.com>
2024-05-12 15:39:19 -0300

    Implementando People Repository Parte 1

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,4 +1,5 @@
 import pytest
+from src.models.sqlite.repositories.people_repository import PeopleRepository
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
 
@@ -16,3 +17,14 @@ def test_delete_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_insert_person():
+    first_name = "test name"
+    last_name = "test last"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
